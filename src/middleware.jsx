7842c7a2e84a9995be0a41a7363c5137903cc48e
Pasit <pasitgut.dev@gmail.com>
2025-03-10 23:04:53 +0700

    fixx

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -97,7 +97,7 @@ console.log("orderIdFromUrl (as string): ", orderIdFromUrl);
       
       if (tableStatus === 'Occupied') {
         console.log('In Occupied')
-        if (orderIdFromUrl != null || orderIdFromUrl != undefined || orderIdFromUrl != 'null') {
+        if (orderIdFromUrl != null || orderIdFromUrl != undefined || orderIdFromUrl.trim() != 'null') {
           // check order status 
           console.log("Order ID From Url ", typeof orderIdFromUrl);
           const orderStatus = await axios.get(`https://api.pasitlab.com/orders/status/${orderIdFromUrl}`);
